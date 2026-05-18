d2e6c4c5eee41ca04c37611ce5db28f021f5bd39
durveshpathak1989 <durveshpathak@gmail.com>
2026-05-18 16:30:16 -0400

    05/18/2026 - Commit for fixing the WiFi Telemetry stack t0 12288

--- RC_FlightController.ino
+++ RC_FlightController.ino
@@ -661,7 +661,7 @@ void setup()
 
     xTaskCreatePinnedToCore(taskRC,    "RC",    6144,nullptr,3,&hTaskRC,    0);
     xTaskCreatePinnedToCore(taskSerial,"Serial",4096,nullptr,1,&hTaskSerial,0);
-    xTaskCreatePinnedToCore(taskWiFi,  "WiFi", 10240,nullptr,1,&hTaskWiFi,  0);
+    xTaskCreatePinnedToCore(taskWiFi,  "WiFi", 12288,nullptr,1,&hTaskWiFi,  0);
     xTaskCreatePinnedToCore(taskBMP,   "BMP280",3072,nullptr,1,&hTaskBMP,   0);
     xTaskCreatePinnedToCore(taskCPU,   "CPU",   3072,nullptr,1,&hTaskCPU,   0);
     xTaskCreatePinnedToCore(taskIMU,   "IMU",   8192,nullptr,5,&hTaskIMU,   1);
